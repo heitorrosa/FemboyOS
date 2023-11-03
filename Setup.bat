e773e92c8041b9bfcfe33783e9d82a4343388e8e
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2023-11-02 23:14:18 -0300

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -22,21 +22,6 @@ rmdir C:\Windows\system32\adminrightstest >NUL 2>&1
 POWERSHELL Set-ExecutionPolicy Unrestricted >NUL 2>&1
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f. >NUL 2>&1
 
-set dependencies=0
-
-if %dependencies%==0 (
-  goto start
-)
-if %dependencies%==2 (
-  goto reboot2
-)
-if %dependencies%==3 (
-  goto reboot3
-)
-if %dependencies%==4 (
-  goto reboot4
-)
-
 :start
 ECHO Installing Thorimum Browser...
 POWERSHELL $ProgressPreference-'SilentlyContinue' >NUL 2>&1
