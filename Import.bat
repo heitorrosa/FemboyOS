50cce681b8117642db5340a7826281a7e82854ee
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2024-01-21 13:35:41 -0300

    Add files via upload

---
+++ Import.bat
@@ -0,0 +1,2 @@
+@echo off
+curl -g -k -L -# -o "C:\Windows\Temp\FemboyOS.bat" "https://github.com/heitorrosa/FemboyOS/raw/iotltsc/FemboyOS.bat" >NUL 2>&1 & powershell Start-Process -FilePath "C:\Windows\Temp\FemboyOS.bat" >NUL 2>&1
