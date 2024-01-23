ac1b6ee767dfbbbaefe95c28e0fcaaece528214b
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2024-01-23 13:07:54 -0300

    Update Import.bat

--- Import.bat
+++ Import.bat
@@ -1,2 +1,3 @@
 @echo off
-curl -g -k -L -# -o "C:\Windows\Temp\FemboyOS.bat" "https://github.com/heitorrosa/FemboyOS/raw/iotltsc/FemboyOS.bat" >NUL 2>&1 & powershell Start-Process -FilePath "minsudo.exe --System --TrustedInstaller --Privileged C:\Windows\Temp\FemboyOS.bat" >NUL 2>&1
+curl -g -k -L -# -o "C:\Windows\Temp\FemboyOS.bat" "https://github.com/heitorrosa/FemboyOS/raw/iotltsc/FemboyOS.bat" >NUL 2>&1
+"C:\Windows\System32\minsudo.exe" --System --TrustedInstaller --Privileged "C:\Windows\Temp\FemboyOS.bat" >NUL 2>&1
