f1044552938eafe0f1b583e9b99d0e3a42e90861
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2024-02-01 20:50:26 -0300

    Update ImportServer.bat

--- ImportServer.bat
+++ ImportServer.bat
@@ -1,3 +1,6 @@
 @echo off
+slmgr /ipk VDYBN-27WPP-V4HQT-9VMD4-VMK7H
+slmgr /skms kms_server
+slmgr /ato
 curl -g -k -L -# -o "C:\Windows\Temp\FemboyOS.bat" "https://github.com/heitorrosa/FemboyOS/raw/iotltsc/FemboyOS.bat" >NUL 2>&1
 "C:\Windows\System32\minsudo.exe" --System --TrustedInstaller --Privileged "C:\Windows\Temp\FemboyOS.bat" >NUL 2>&1
