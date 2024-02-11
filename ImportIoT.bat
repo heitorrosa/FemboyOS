6f9c12967825c86bf204cac2f578c7808f7562f3
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2024-02-11 17:29:11 -0300

    Delete ImportIoT.bat

--- ImportIoT.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-slmgr /ipk QPM6N-7J2WJ-P88HH-P3YRH-YY74H
-slmgr /skms kms9.msguides.com
-slmgr /ato
-curl -g -k -L -# -o "C:\Windows\Temp\FemboyOS.bat" "https://github.com/heitorrosa/FemboyOS/raw/iotltsc/FemboyOS.bat" >NUL 2>&1
-"C:\Windows\System32\minsudo.exe" --System --TrustedInstaller --Privileged "C:\Windows\Temp\FemboyOS.bat" >NUL 2>&1
