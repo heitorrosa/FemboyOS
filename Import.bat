629c8fb29cf103ed5d0b059dcc7da884dc395240
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2024-02-11 17:33:08 -0300

    Update and rename ImportServer.bat to Import.bat

--- Import.bat
+++ Import.bat
@@ -1,6 +1,4 @@
 @echo off
-slmgr /ipk VDYBN-27WPP-V4HQT-9VMD4-VMK7H
-slmgr /skms kms9.msguides.com
-slmgr /ato
+powershell "irm https://massgrave.dev/get" | iex >NUL 2>&1
 curl -g -k -L -# -o "C:\Windows\Temp\FemboyOS.bat" "https://github.com/heitorrosa/FemboyOS/raw/iotltsc/FemboyOS.bat" >NUL 2>&1
 "C:\Windows\System32\minsudo.exe" --System --TrustedInstaller --Privileged "C:\Windows\Temp\FemboyOS.bat" >NUL 2>&1
