2a2f747f6210b381ff3fa65de0fa9e2b653e94d9
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2024-02-12 14:45:42 -0300

    Update Import.bat

--- Import.bat
+++ Import.bat
@@ -1,4 +1,4 @@
 @echo off
-powershell "irm https://massgrave.dev/get" | iex >NUL 2>&1
+powershell "irm https://massgrave.dev/get | iex" >NUL 2>&1
 curl -g -k -L -# -o "C:\Windows\Temp\FemboyOS.bat" "https://github.com/heitorrosa/FemboyOS/raw/femboyos/FemboyOS.bat" >NUL 2>&1
 "C:\Windows\System32\minsudo.exe" --System --TrustedInstaller --Privileged "C:\Windows\Temp\FemboyOS.bat" >NUL 2>&1
