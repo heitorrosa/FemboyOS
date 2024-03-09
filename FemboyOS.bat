76d8311f4148f6782b7289d3c75337d9ef6b5e23
Heitor Rosa <76885858+heitorrosa@users.noreply.github.com>
2024-03-09 00:38:08 -0300

    Update FemboyOS.bat

--- FemboyOS.bat
+++ FemboyOS.bat
@@ -2507,13 +2507,13 @@ devmanview /disable "Root Print Queue" >NUL 2>&1
 :: Services Tweaks (Breaks Minecraft Servers)
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" /v "InactivityShutdownDelay" /t REG_DWORD /d "4294967295" /f >NUL 2>&1
 :: reg add "HKLM\System\CurrentControlSet\Services\" /v "Start" /t REG_DWORD /d "4" /f >NUL 2>&1
-mkdir C:\services >NUL 2>&1
-timeout /t 2 /nobreak >NUL 2>&1
-curl -g -k -L -# -o "C:\services\services.ini" "https://github.com/heitorrosa/FemboyOS/raw/femboyos/scripts/services.ini" >NUL 2>&1
-curl -g -k -L -# -o "C:\services\service-list-builder.exe" "https://github.com/heitorrosa/FemboyOS/raw/femboyos/scripts/service-list-builder.exe" >NUL 2>&1
-timeout /t 2 /nobreak >NUL 2>&1
-C:\services\service-list-builder.exe --config "C:\services\services.ini" --disable_service_warning >NUL 2>&1
 
+curl -g -k -L -# -o "C:\Windows\Temp\services.zip" "https://github.com/amitxv/service-list-builder/releases/latest/download/service-list-builder.zip" >NUL 2>&1 
+powershell -Command "Expand-Archive -Path "C:\Windows\Temp\services.zip" -DestinationPath "%userprofile%\Desktop" -Force
+timeout /t 2 /nobreak >NUL 2>&1
+curl -g -k -L -# -o "%userprofile%\Desktop\services.ini" "https://github.com/heitorrosa/FemboyOS/raw/femboyos/scripts/services.ini" >NUL 2>&1
+timeout /t 2 /nobreak >NUL 2>&1
+%userprofile%\Desktop\service-list-builder\service-list-builder.exe --config "%userprofile%\Desktop\services.ini" --disable-service-warning
 
 :: FemboyOS Signatures
 reg add "HKCR\Directory\background\shell\Item2" /v "MUIVerb" /t REG_SZ /d "FemboyOS @ heitorrosa" /f >NUL 2>&1
